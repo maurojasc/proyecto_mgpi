4a4da961f308c79b21d30cb4b66fef947ca672e2
Hernan Javier <Hernan Javier@DESKTOP-7ONSDUU>
2019-07-09 21:15:40 -0500

    Prueba de mensaje 2 a

--- ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
+++ ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
@@ -5,18 +5,46 @@
  */
 package ing_sw_2019;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Hernan Javier
  */
 public class Ing_sw_2019 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.printnl("Prueba desde Git");
+        int numero = leerNumero();
+        while (numero > -1) {
+            mostrarFactorial(numero);
+            numero = leerNumero();
+        }
+        System.out.println("\nFin del programa");
     }
-    
+
+    private static void mostrarFactorial(int num) {
+        System.out.printf("%n%d! = ", num);
+        int fact = 1;
+        if (num != 0) {
+            for (int i = num; i > 0; i--) {
+                fact *= i;
+                System.out.print(i);
+                if (i > 1) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print(" = ");
+                }
+            }
+        }
+        System.out.println(fact + "\n");
+    }
+
+    private static int leerNumero() {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número (negativo para terminar): ");
+        num = sc.nextInt();
+        return num;
+    }
+
 }
