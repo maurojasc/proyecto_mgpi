7a4ef4651b1c5f7d0a3d9f84ba79d82ac595392a
maurojasc <33296864+maurojasc@users.noreply.github.com>
2019-07-10 17:45:04 -0500

    Update Ing_sw_2019.java
    revisión Mauricio Rojas C.

--- ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
+++ ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
@@ -21,6 +21,7 @@ public class Ing_sw_2019 {
             numero = leerNumero();
         }
         System.out.println("\nFin del programa");
+        System.out.println.("revisado Mauricio Rojas");
     }
 
     private static void mostrarFactorial(int num) {
