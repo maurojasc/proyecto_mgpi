629fe6e584901838a583b40eaaf7a64fd9fa03ef
HernanGuio <51929663+HernanGuio@users.noreply.github.com>
2019-07-10 18:12:57 -0500

    Update Ing_sw_2019.java

--- ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
+++ ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
@@ -21,7 +21,7 @@ public class Ing_sw_2019 {
             numero = leerNumero();
         }
         System.out.println("\nFin del programa");
-        System.out.println("\nActualizacion");
+        System.out.println("\nActualizacion").x;
     }
 
     private static void mostrarFactorial(int num) {
